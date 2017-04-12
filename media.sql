248c5fb506d853d5d81e80627e90044770b25625
Clifford Grimmell <agrimmell2@gmail.com>
2017-04-12 10:29:34 -0700

    add database

---
+++ media.sql
@@ -0,0 +1,364 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.2
+-- Dumped by pg_dump version 9.6.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: animals; Type: TABLE; Schema: public; Owner: clifford
+--
+
+CREATE TABLE animals (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE animals OWNER TO clifford;
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: clifford
+--
+
+CREATE SEQUENCE animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE animals_id_seq OWNER TO clifford;
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: clifford
+--
+
+ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
+
+
+--
+-- Name: endangered_animals; Type: TABLE; Schema: public; Owner: clifford
+--
+
+CREATE TABLE endangered_animals (
+    id integer NOT NULL,
+    name character varying,
+    health character varying,
+    age character varying
+);
+
+
+ALTER TABLE endangered_animals OWNER TO clifford;
+
+--
+-- Name: endangered_animals_id_seq; Type: SEQUENCE; Schema: public; Owner: clifford
+--
+
+CREATE SEQUENCE endangered_animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE endangered_animals_id_seq OWNER TO clifford;
+
+--
+-- Name: endangered_animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: clifford
+--
+
+ALTER SEQUENCE endangered_animals_id_seq OWNED BY endangered_animals.id;
+
+
+--
+-- Name: locations; Type: TABLE; Schema: public; Owner: clifford
+--
+
+CREATE TABLE locations (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE locations OWNER TO clifford;
+
+--
+-- Name: locations_id_seq; Type: SEQUENCE; Schema: public; Owner: clifford
+--
+
+CREATE SEQUENCE locations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE locations_id_seq OWNER TO clifford;
+
+--
+-- Name: locations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: clifford
+--
+
+ALTER SEQUENCE locations_id_seq OWNED BY locations.id;
+
+
+--
+-- Name: rangers; Type: TABLE; Schema: public; Owner: clifford
+--
+
+CREATE TABLE rangers (
+    id integer NOT NULL,
+    name character varying,
+    phonenumber character varying
+);
+
+
+ALTER TABLE rangers OWNER TO clifford;
+
+--
+-- Name: rangers_id_seq; Type: SEQUENCE; Schema: public; Owner: clifford
+--
+
+CREATE SEQUENCE rangers_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE rangers_id_seq OWNER TO clifford;
+
+--
+-- Name: rangers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: clifford
+--
+
+ALTER SEQUENCE rangers_id_seq OWNED BY rangers.id;
+
+
+--
+-- Name: sightings; Type: TABLE; Schema: public; Owner: clifford
+--
+
+CREATE TABLE sightings (
+    id integer NOT NULL,
+    animal_id integer,
+    location character varying,
+    ranger_name character varying
+);
+
+
+ALTER TABLE sightings OWNER TO clifford;
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE; Schema: public; Owner: clifford
+--
+
+CREATE SEQUENCE sightings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE sightings_id_seq OWNER TO clifford;
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: clifford
+--
+
+ALTER SEQUENCE sightings_id_seq OWNED BY sightings.id;
+
+
+--
+-- Name: animals id; Type: DEFAULT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq'::regclass);
+
+
+--
+-- Name: endangered_animals id; Type: DEFAULT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY endangered_animals ALTER COLUMN id SET DEFAULT nextval('endangered_animals_id_seq'::regclass);
+
+
+--
+-- Name: locations id; Type: DEFAULT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY locations ALTER COLUMN id SET DEFAULT nextval('locations_id_seq'::regclass);
+
+
+--
+-- Name: rangers id; Type: DEFAULT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY rangers ALTER COLUMN id SET DEFAULT nextval('rangers_id_seq'::regclass);
+
+
+--
+-- Name: sightings id; Type: DEFAULT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY sightings ALTER COLUMN id SET DEFAULT nextval('sightings_id_seq'::regclass);
+
+
+--
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: clifford
+--
+
+COPY animals (id, name) FROM stdin;
+2	Cow
+\.
+
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: clifford
+--
+
+SELECT pg_catalog.setval('animals_id_seq', 2, true);
+
+
+--
+-- Data for Name: endangered_animals; Type: TABLE DATA; Schema: public; Owner: clifford
+--
+
+COPY endangered_animals (id, name, health, age) FROM stdin;
+1	Tiger	Ill	Newborn
+\.
+
+
+--
+-- Name: endangered_animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: clifford
+--
+
+SELECT pg_catalog.setval('endangered_animals_id_seq', 1, true);
+
+
+--
+-- Data for Name: locations; Type: TABLE DATA; Schema: public; Owner: clifford
+--
+
+COPY locations (id, name) FROM stdin;
+\.
+
+
+--
+-- Name: locations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: clifford
+--
+
+SELECT pg_catalog.setval('locations_id_seq', 1, false);
+
+
+--
+-- Data for Name: rangers; Type: TABLE DATA; Schema: public; Owner: clifford
+--
+
+COPY rangers (id, name, phonenumber) FROM stdin;
+\.
+
+
+--
+-- Name: rangers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: clifford
+--
+
+SELECT pg_catalog.setval('rangers_id_seq', 1, false);
+
+
+--
+-- Data for Name: sightings; Type: TABLE DATA; Schema: public; Owner: clifford
+--
+
+COPY sightings (id, animal_id, location, ranger_name) FROM stdin;
+1	1	asd	asfx
+2	1	asdf	asdf
+\.
+
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: clifford
+--
+
+SELECT pg_catalog.setval('sightings_id_seq', 2, true);
+
+
+--
+-- Name: animals animals_pkey; Type: CONSTRAINT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY animals
+    ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: endangered_animals endangered_animals_pkey; Type: CONSTRAINT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY endangered_animals
+    ADD CONSTRAINT endangered_animals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: locations locations_pkey; Type: CONSTRAINT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY locations
+    ADD CONSTRAINT locations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rangers rangers_pkey; Type: CONSTRAINT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY rangers
+    ADD CONSTRAINT rangers_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sightings sightings_pkey; Type: CONSTRAINT; Schema: public; Owner: clifford
+--
+
+ALTER TABLE ONLY sightings
+    ADD CONSTRAINT sightings_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
